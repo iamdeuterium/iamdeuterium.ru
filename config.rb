02e4916788aad0f320d71c448ed58528d4562fc0
Stanislav Fesenko <iamdeuterium@gmail.com>
2015-06-03 00:47:56 +0300

    Initial commit

---
+++ config.rb
@@ -0,0 +1,24 @@
+configure :development do
+  activate :livereload
+end
+
+set :port, 3000
+
+set :css_dir,     'stylesheets'
+set :js_dir,      'javascripts'
+set :images_dir,  'images'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+
+  activate :asset_hash
+end
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = 'iamdeuterium.ru'
+  deploy.user   = 'tz'
+  deploy.path   = "/home/#{deploy.user}/projects/iamdeuterium.ru"
+  deploy.clean  = true
+end
